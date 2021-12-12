87394dd19ca01944e717da03c7bd98714c56d02a
Arcewirz <xxxAdrian2000x@gmail.com>
2021-12-12 20:13:29 +0100

    Update experiments2.R

--- scripts/experiments2.R
+++ scripts/experiments2.R
@@ -1,5 +1,9 @@
 library(ggplot2)
 library(dplyr)
-
+library(car)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))  # set working dir to current folder
 df <- read.csv("../data/cleaned_nba_dataset.csv")  # the same df as in main.rnw after cleaning
+
+qqPlot(df$PTS, distribution="gamma", shape=1.5)
+
+ks.test(x=df$REB, y="pgamma", 1.5, 1)
