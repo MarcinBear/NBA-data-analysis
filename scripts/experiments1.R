c4e8de58bdf55ff8b38f41c20351d1f5bcb177c7
Marcin Miśkiewicz <>
2021-12-13 12:45:51 +0100

    new plot added

--- scripts/experiments1.R
+++ scripts/experiments1.R
@@ -4,6 +4,7 @@ library(dplyr)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))  # set working dir to current folder
 df <- read.csv("../data/cleaned_nba_dataset.csv")  # the same df as in main.rnw after cleaning
 summary(df)
+MY_PALETTE1 = c("#ff0000", "#ffa200", "#007bff", "#ff00fb", "#241aa5")
 
 existing_teams <- c('Lakers', 'Clippers', 'Kings', 'Rockets', 'Nuggets', 'Thunder',
                     'Trail Blazers', 'Bucks', 'Warriors', 'Knicks', 'Magic', 'Suns',
@@ -33,3 +34,19 @@ ggplot(data=df, aes(x=SEASON_EXP, y=PTS, group=SEASON_EXP, fill=SEASON_EXP)) +
        y = "średnia liczba punktów na mecz", 
        title = "Liczba punktów vs. doświadczenie w lidze")#+
   # theme(text = element_text(size=35))
+
+df2 <- df[!(df$POSITION == ""), ]
+
+ggplot(df2, aes(AST, REB, shape=POSITION, color=POSITION, na.rm=TRUE, label=PLAYER)) +
+  geom_point(size=4, stroke=1.4, alpha=0.4, na.rm=TRUE) +
+  geom_smooth(method = "lm", show.legend = FALSE, na.rm=TRUE) +
+  geom_label_repel(data = subset(df2, AST*REB > 80), box.padding = 5, 
+                   point.padding = 0) +
+  
+  scale_color_manual(values=MY_PALETTE1[1:3]) +
+  scale_shape_manual(values=c(1, 2, 5)) +
+  guides(color = guide_legend(override.aes = list(size = 10))) +
+  labs(x = "asysty / mecz",
+       y = "zbiórki / mecz",
+       title = "Zbiórki vs. asysty",
+       subtitle = sprintf("Wykres rozrzutu z naniesionymi liniami trendu"))
